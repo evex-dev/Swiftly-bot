c32a02b26e5edf2e129472e934c765e9c952e8f1
371tti <109227273+371tti@users.noreply.github.com>
2025-02-26 16:29:37 +0900

    feat: 投票結果のパーセンテージ計算を修正し、最大投票数に基づくように変更

--- cogs/poll.py
+++ cogs/poll.py
@@ -286,9 +286,10 @@ class Poll(commands.Cog):
                     color=discord.Color.green()
                 )
 
+                max_votes_count = max(vote_counts.values())
                 for i, option in enumerate(options):
                     votes = vote_counts.get(i, 0)
-                    percentage = (votes / total_votes * 100) if total_votes > 0 else 0
+                    percentage = (votes / max_votes_count * 100) if total_votes > 0 else 0
                     bar_length = int(percentage / 5)  # 20文字を最大とする
                     progress_bar = '█' * bar_length + '_' * (20 - bar_length)
                     embed.add_field(
