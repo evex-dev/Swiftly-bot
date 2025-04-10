599b836fe34aeaaa50041464e2cbade2e0f9c967
techfish <msuke1018@icloud.com>
2025-04-10 23:21:42 +0900

    feat: メッセージリンク機能の改善 - フッター情報を追加し、色を変更

--- cogs/system/messagelink.py
+++ cogs/system/messagelink.py
@@ -8,7 +8,6 @@ class MessageLink(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        # メッセージリンクの正規表現
         link_pattern = r"https://discord\.com/channels/(\d+)/(\d+)/(\d+)"
         match = re.search(link_pattern, message.content)
         if match:
@@ -23,11 +22,14 @@ class MessageLink(commands.Cog):
                 target_message = await channel.fetch_message(message_id)
                 embed = discord.Embed(
                     description=target_message.content,
-                    color=discord.Color.blue()
+                    color=discord.Color.light_blue()
                 )
                 embed.set_author(
                     name=target_message.author.display_name,
                     icon_url=target_message.author.avatar.url if target_message.author.avatar else None
+                )
+                embed.set_footer(
+                    text=f"Sent on {target_message.created_at.strftime('%Y-%m-%d %H:%M:%S')} in {guild.name}"
                 )
                 await message.channel.send(embed=embed)
             except Exception as e:
